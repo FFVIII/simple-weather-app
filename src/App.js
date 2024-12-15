59417534e456f4981bd6fbc02271db57079cfc67
FFVIII <fangvsyu@gmail.com>
2024-12-15 00:14:13 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+function App() {
+  
+  const [city, setCity] = useState('');
+  const [weather, setWeather] = useState('');
+  const apiKey = 'c9bb642837bf2cf57629bca1d21c3b62';
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+  const fetchWeather = async () => {
+    if(!city) return;
+    const response = await fetch(url);
+    const data = await response.json();
+    setWeather(data);
+  }
+
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <div>
+        <input 
+          type="text"
+          placeholder="Enter city name"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        />
+         <button onClick={fetchWeather}>Get Weather</button>
+      </div>
+      { weather && (
+        <div className="weather-info">
+          <h2>In {weather.name}</h2>
+          <p>Temperature: {Math.round(weather.main.temp - 273.15)}°C</p>
+          <p>Condition: {weather.weather[0].description}</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
